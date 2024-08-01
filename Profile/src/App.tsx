2e41e82d91d88674747c88608e55a0f7a8caa5e3
97Fangz <briansegem25@gmail.com>
2024-08-01 16:31:37 +0300

    "i"

--- Profile/src/App.tsx
+++ Profile/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, git } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './pages/git';
