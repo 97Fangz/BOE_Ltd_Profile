6385d0ccd5460565b7d7ba5718baf9c1648b2f37
97Fangz <briansegem25@gmail.com>
2024-08-03 19:49:30 +0300

    " "

--- Profile/src/pages/Contact.tsx
+++ Profile/src/pages/Contact.tsx
@@ -47,9 +47,25 @@ const Contact: React.FC = () => {
       <h3 className="text-sm font-semibold text-blue-900 mb-4">Contact us via the following:</h3>
       <ContactContainer>
         <ContactItem>
-          <i className="fa fa-phone"></i>
-          <p>+254798877813,
-             +254715119636</p>
+          <button
+  class="middle none center w-full rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+  data-ripple-light="true">
+  Button
+</button>
+<div class="w-full pt-5 px-4 mb-8 mx-auto ">
+    <div class="text-sm text-gray-700 py-1">
+        Made with <a class="text-gray-700 font-semibold" href="https://www.material-tailwind.com/docs/html/button?ref=tailwindcomponents" target="_blank">Material Tailwind</a> by <a href="https://www.creative-tim.com?ref=tailwindcomponents" class="text-gray-700 font-semibold" target="_blank"> Creative Tim</a>.
+    </div>
+</div>
+
+<!-- stylesheet -->
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
+/>
+
+<!-- Ripple Effect from cdn -->
+<script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
         </ContactItem>
         <ContactItem>
           <i className="fa-solid fa-envelope"></i>
