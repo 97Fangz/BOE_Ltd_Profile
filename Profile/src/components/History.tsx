e3fb59a93441b9bad527f860863d39554aa3240f
97Fangz <briansegem25@gmail.com>
2024-08-04 22:18:15 +0300

    ""

--- Profile/src/components/History.tsx
+++ Profile/src/components/History.tsx
@@ -6,7 +6,7 @@ const History = () => {
         <span className="my-4 border-l-8 border-gray-800 tracking-wide flex items-start pl-4  justify-start h-max">MG has established a long-term commercial relationship with reputable and businesssavvy companies. In this instance, we anticipate that companies to fulfill the commitment
         before shipment of theAU with CIF method.</span>
        <p>
-       BOE Limited was founded in 2023 by Joel Smith. Since then, we have become a leading provider of high-quality minerals. We have a reputation for excellence and
+       BOE Limited was founded in 2023 by Joel Kipyego. Since then, we have become a leading provider of high-quality minerals. We have a reputation for excellence and
        integrity. We are committed to providing our customers with the best products and services.
        </p>
     </div>
