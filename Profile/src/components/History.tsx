9c8ae3cc2f7cb715b24574e68732e5424e1a98b2
97Fangz <briansegem25@gmail.com>
2024-08-04 22:20:01 +0300

    ""

--- Profile/src/components/History.tsx
+++ Profile/src/components/History.tsx
@@ -3,8 +3,8 @@ const History = () => {
   return (
     <div className="bg-[whitesmoke] my-[5%] mx-[5%] px-4 rounded shadow-md">
         <h1 className="border-b-2 w-max mx-auto text-2xl font-semibold text-amber-900 border-amber-900">Our History</h1>
-        <span className="my-4 border-l-8 border-gray-800 tracking-wide flex items-start pl-4  justify-start h-max">MG has established a long-term commercial relationship with reputable and businesssavvy companies. In this instance, we anticipate that companies to fulfill the commitment
-        before shipment of theAU with CIF method.</span>
+        <span className="my-4 border-l-8 border-gray-800 tracking-wide flex items-start pl-4  justify-start h-max"> has established a long-term commercial relationship with reputable and business savvy companies. In this instance, we anticipate that companies to fulfill the commitment
+        before shipment of the AU with CIF method.</span>
        <p>
        BOE Limited was founded in 2023 by Joel Chesiro. Since then, we have become a leading provider of high-quality minerals. We have a reputation for excellence and
        integrity. We are committed to providing our customers with the best products and services.
