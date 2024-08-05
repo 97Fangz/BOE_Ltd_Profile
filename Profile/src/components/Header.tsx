12fad81cf3738d289c28a2824d6539358628af8b
97Fangz <briansegem25@gmail.com>
2024-08-05 10:01:44 +0300

    ""

--- Profile/src/components/Header.tsx
+++ Profile/src/components/Header.tsx
@@ -76,9 +76,7 @@ const Header: React.FC = () => {
             <NavLink 
               to="/services"
               className={({ isActive }) => `${isActive ? "border-b-2 font-semibold border-gray-800 bg-[white] py-1 px-2" : ""}`}>
-              Services
-              <i className=""></i>
-            </NavLink>
+              Services avLink>
             <NavLink 
               to="/contact"
               className={({ isActive }) => `${isActive ? "border-b-2 font-semibold border-gray-800 bg-[white] py-1 px-2" : ""} flex items-center justify-center gap-2`}>
