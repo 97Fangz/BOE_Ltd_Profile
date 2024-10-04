d23232a20f2b3772cc5c34196db81dbe5abee8f0
97Fangz <briansegem25@gmail.com>
2024-10-04 19:50:08 +0300

    "i"

--- Profile/src/pages/Contact/ContactSection.tsx
+++ Profile/src/pages/Contact/ContactSection.tsx
@@ -1,4 +1,4 @@
-mport React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Phone, Mail, MapPin, Instagram, TikTok } from 'lucide-react';
