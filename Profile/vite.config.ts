82be744a1bbc0f62167403c226fd472c06937e57
97Fangz <briansegem25@gmail.com>
2024-10-06 02:04:22 +0300

    Deployment_error

--- Profile/vite.config.ts
+++ Profile/vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    react(),
+    react(), // React plugin for Vite
+    tsconfigPaths() // Allows TypeScript path aliases
   ],
   resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': '/src' // Alias @ to point to the src folder
+    }
+  },
+  server: {
+    port: 3000, // You can specify your desired port
+    open: true, // Automatically opens the app in the browser when the server starts
   },
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html', // Specify the entry point
-      },
-    },
-  },
+    outDir: 'dist', // Specify the output directory for build files
+  }
 });
