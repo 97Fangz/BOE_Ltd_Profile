9c66d5718927fbedc9392efc98c9ee88215fbce9
97Fangz <briansegem25@gmail.com>
2024-10-05 21:10:28 +0300

    " {"

--- Profile/vite.config.ts
+++ Profile/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  //base: '/BOE_Ltd_Profile/', //repository name here
-})
+  plugins: [react(), tsconfigPaths()],
+});
