e6a3f6831a9a153271acdb5c48c4982c127e55a4
97Fangz <briansegem25@gmail.com>
2024-10-05 20:31:41 +0300

    "interface"

--- Profile/src/pages/Contact/ContactSection.tsx
+++ Profile/src/pages/Contact/ContactSection.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 import { Camera, Smartphone, Mail, MapPin, Instagram, Video } from 'lucide-react';
 
-const ContactCard = ({ icon: Icon, title, content, buttonText, buttonHref }) => {
+interface ContactCardProps {
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  title: string;
+  content: string[];
+  buttonText: string;
+  buttonHref: string;
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({ icon: Icon, title, content, buttonText, buttonHref }) => {
   const cardRef = useRef(null);
 
   useEffect(() => {
