71c6bb9ef7e0f23d44cbbfa197ff8362b1820581
97Fangz <briansegem25@gmail.com>
2024-10-06 02:12:12 +0300

    Fixed

--- Profile/vite.config.ts
+++ Profile/vite.config.ts
@@ -4,19 +4,17 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    react(), // React plugin for Vite
-    tsconfigPaths() // Allows TypeScript path aliases
+    react(),
+    tsconfigPaths()
   ],
   resolve: {
     alias: {
-      '@': '/src' // Alias @ to point to the src folder
+      '@': '/src'
     }
   },
-  server: {
-    port: 3000, // You can specify your desired port
-    open: true, // Automatically opens the app in the browser when the server starts
-  },
   build: {
-    outDir: 'dist', // Specify the output directory for build files
+    rollupOptions: {
+      external: ['framer-motion']
+    }
   }
 });
