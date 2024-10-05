48ae413d47b1a09f82846cd6647093f9411b5c12
97Fangz <briansegem25@gmail.com>
2024-10-06 00:24:38 +0300

    fixed production issues

--- Profile/vite.config.ts
+++ Profile/vite.config.ts
@@ -3,10 +3,20 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), // React plugin for Vite
+    tsconfigPaths() // Allows TypeScript path aliases
+  ],
   resolve: {
     alias: {
-      '@': '/src' // Ensures @ points to the src folder
+      '@': '/src' // Alias @ to point to the src folder
     }
+  },
+  server: {
+    port: 3000, // You can specify your desired port
+    open: true, // Automatically opens the app in the browser when the server starts
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for build files
   }
 });
