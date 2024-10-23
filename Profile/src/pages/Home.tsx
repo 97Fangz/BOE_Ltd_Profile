9e3d7e66c94e220521783fc4c2cfbd1c2b50de03
97Fangz <briansegem25@gmail.com>
2024-10-23 16:54:05 +0300

    ""

--- Profile/src/pages/Home.tsx
+++ Profile/src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { ChevronDown, ChevronUp, Phone, Mail, MapPin, ChevronLeft, ChevronRight, Award, Shield, TrendingUp } from 'lucide-react';
 import CompanyInfo from '../components/CompanyInfo';
-
 import History from '../components/History';
 import IndustriesSection from '../components/IndustriesSection';
 
