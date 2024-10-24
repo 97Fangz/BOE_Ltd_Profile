f0cd84906f0b50253fd730afc16e7bc0fe434d9f
97Fangz <briansegem25@gmail.com>
2024-10-24 14:33:02 +0300

    ""

--- Profile/src/pages/Home.tsx
+++ Profile/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { ChevronDown, ChevronUp, Phone, Mail, MapPin, ChevronLeft, ChevronRight, Award, Shield, TrendingUp } from 'lucide-react';
-import CompanyInfo from './pages/components/CompanyInfo';
+import CompanyInfo from './../components/CompanyInfo';
 import History from './components/History';
 
 // Testimonials data remains the same
