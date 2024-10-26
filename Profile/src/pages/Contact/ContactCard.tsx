8306fc7b4f13298ddeb3917931d3944e84cea074
97Fangz <briansegem25@gmail.com>
2024-10-26 17:38:40 +0300

    " {"

--- Profile/src/pages/Contact/ContactCard.tsx
+++ Profile/src/pages/Contact/ContactCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardContent } from '../components/ui/card';
-import { Button } from '../components/ui/button';
+import { Card, CardContent } from '../../components/ui/card';
+import { Button } from './components/ui/button';
 import { Phone, Mail, MapPin, Instagram, User as TikTok } from 'lucide-react';
 
 interface ContactData {
